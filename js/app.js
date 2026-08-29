ab0e255cf1f35381726aba46828ee0ea605cc280
mihomatsuoka <matsuoka-31fj@g.bears.ed.jp>
2026-08-30 00:13:03 +0900

    Add quest modal open and close functions

--- js/app.js
+++ js/app.js
@@ -5,3 +5,11 @@ function startGuild() {
     document.getElementById("guild-home").style.display = "block";
 
 }
+
+function openQuest() {
+    document.getElementById("quest-modal").style.display = "flex";
+}
+
+function closeQuest() {
+    document.getElementById("quest-modal").style.display = "none";
+}
